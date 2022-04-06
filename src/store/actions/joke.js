605e2f23f2753f7a6e94694734d2b8713ba3fb78
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2022-04-06 15:53:12 +0200

    joke status message improved

--- src/store/actions/joke.js
+++ src/store/actions/joke.js
@@ -7,7 +7,7 @@ export const GET_JOKE_FAIL = "GET_JOKE_FAIL";
 export const getJokeRequest = () => {
     return {
         type: GET_JOKE_REQUEST,
-        text: "Waiting...",
+        text: "Waiting for the joke...",
     };
 };
 
@@ -21,13 +21,13 @@ export const getJokeSuccess = (joke = "New joke !") => {
 export const getJokeFail = () => {
     return {
         type: GET_JOKE_FAIL,
-        text: "Impossible to get the joke... :'(",
+        text: "Error when getting the joke... :'(",
     };
 };
 
 export const getJoke = () => {
     return dispatch => {
-        dispatch(getJokeSuccess());
+        dispatch(getJokeRequest());
         axios.get("https://api.jokes.one/jod?category=animal")
             .then((v) => {
                 dispatch(getJokeSuccess(v.data.contents.jokes[0].joke.text));
